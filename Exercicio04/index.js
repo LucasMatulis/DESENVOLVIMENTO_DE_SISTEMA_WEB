aa1edd9800f85a48d17117f1d6f741c737cb09e1
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2023-08-21 11:12:18 -0300

    ç´kl

---
+++ Exercicio04/index.js
@@ -0,0 +1,6 @@
+document.write("<b class='teste'>eu estou no corpo do documento</b>");
+alert("aiiiiiiii zé da manga");
+let confirma= confirm("é memo???")
+let nome= prompt("Nome???")
+
+alert(this.confirma+" - "+nome)
---
+++ Exercicio05/index.js
@@ -0,0 +1,5 @@
+var salario= prompt("Digite seu salario")
+
+var aumento= parseInt(salario)*1.25
+
+alert(salario+"  "+aumento)
