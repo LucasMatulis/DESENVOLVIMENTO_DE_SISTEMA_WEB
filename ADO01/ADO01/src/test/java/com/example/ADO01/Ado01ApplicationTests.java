45dbf1f4e3c680eeb25a0195e5a5d65a62ca9b5b
Matulis <70170180+LucasMatulis@users.noreply.github.com>
2023-09-20 15:07:27 -0300

    sa

---
+++ ADO01/ADO01/src/test/java/com/example/ADO01/Ado01ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ADO01;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Ado01ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
